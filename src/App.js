2e10d30fdae592c0454c741879d8fcbdf8f5e562
Zach Forrest <ZachForrest@Zachs-MacBook-Pro.local>
2020-02-05 15:42:45 -0500

    It's alive! ready for styling.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,28 @@ let images = [{url: 'https://cdn.motor1.com/images/mgl/8bpn2/s1/2018-porsche-911
 {url: 'https://2p2bboli8d61fqhjiqzb8p1a-wpengine.netdna-ssl.com/wp-content/uploads/2018/08/Porsche-918-Spyder-11-1.jpg', title: 'Porsche 918 Spyder', caption: 'This Porsche sells for over 1M USD'},
 {url: 'https://cdn.bringatrailer.com/wp-content/uploads/2019/03/1977_porsche_911_carrera_3-0_1558726288c0ffb79BE0BBBF-9ED7-40BC-97D6-5AC85CF566C0-940x698.jpg', title: 'Porsche Carrera 3.0', caption: 'A beautiful Porsche from the late 70\'s'}]
 
+function ImageList(props) {
+
+  let images = props.images.map((image, index) => (
+
+    <React.Fragment key={index}>
+       <li>
+        <img src={image.url} alt='submitted' /> 
+        <h2>{image.title}</h2>
+        <p>{image.caption}</p>
+       </li> 
+    </React.Fragment>
+
+  ));
+
+  return (
+    <ul> {images} </ul>
+  )
+}
+
+
+
+
 class ImageForm extends Component {
 
   constructor(props) {
@@ -18,6 +40,7 @@ class ImageForm extends Component {
     this.handleUrl = this.handleUrl.bind(this);
     this.handleTitle = this.handleTitle.bind(this);
     this.handleCaption = this.handleCaption.bind(this);
+    this.newImage = this.newImage.bind(this);
   }
 
   handleUrl(event) {
@@ -53,17 +76,17 @@ class ImageForm extends Component {
   }
 }
 
-class ImageList extends Component {
-  render() {
-    return(
-      <ul>
-        <li> <img src='{this.state.url}' alt='submitted' /> </li>
-    <li> <h2>{this.state.title}</h2> </li>
-        <li> <p>{this.state.caption}</p> </li>
-      </ul>
-    )
-  }
-}
+// class ImageList extends Component {
+//   render() {
+//     return(
+//       <ul>
+//         <li> <img src='{this.state.url}' alt='submitted' /> </li>
+//         <li> <h2>{this.state.title}</h2> </li>
+//         <li> <p>{this.state.caption}</p> </li>
+//       </ul>
+//     )
+//   }
+// }
 
 class ImageBoard extends Component {
 
